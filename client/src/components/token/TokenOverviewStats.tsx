f2d2c3464294582c11761051c8e9481d27d06942
Phuong Le <22120286@student.hcmus.edu.vn>
2026-03-01 12:24:11 +0700

    refactor: simplify import statement for client in TokenOverviewStats component

--- client/src/components/token/TokenOverviewStats.tsx
+++ client/src/components/token/TokenOverviewStats.tsx
@@ -1,4 +1,4 @@
-import { client } from "@/api/main";
+import client from "@/api/main";
 import classNames from "classnames";
 import type { InferResponseType } from "hono/client";
 import { useEffect, useState } from "react";
