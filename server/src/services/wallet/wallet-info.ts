ec34f9b039331adab77efa311afcc5ae0c0f3cb2
hongphuchcmus <22120271@student.hcmus.edu.vn>
2026-03-21 19:46:05 +0700

    refactor: update package.json workspaces structure and remove unused wallet function

--- server/src/services/wallet/wallet-info.ts
+++ server/src/services/wallet/wallet-info.ts
@@ -1,35 +1,3 @@
-import * as bds from "@sv/util/util-bitquery.js";
-import type { BDS_WalletFirstFund } from "../_types/wallet_raw_responses.js";
-
-export async function getWalletFirstFund(wallets: string[]) {
-  const bdsEndpoint = bds.getEndpoint("/wallet/v2/tx/first-funded");
-  const req = new Request(bdsEndpoint, {
-    method: "POST",
-    headers: bds.getRequiredHeaders(),
-    body: JSON.stringify({
-      wallets,
-    }),
-  });
-
-  const resp = await fetch(req);
-  if (!resp.ok) {
-    console.log(resp.status);
-    return null;
-  }
-
-  const res = (await resp.json()) as BDS_WalletFirstFund;
-
-  if (!res.success) {
-    console.log(await resp.json());
-    return null;
-  }
-
-  const addressToFirstFund = Object.fromEntries(
-    Object.entries(res.data).map(([address, data]) => [
-      address,
-      { timestamp: data.block_unix_time },
-    ]),
-  );
-
+export async function getWalletFirstFund(wallet: string) {
   return addressToFirstFund;
 }
